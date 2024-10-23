2a39475efc9ce6cf4d9ef75717ad69566f41a63f
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-10-23 23:41:00 +0300

    task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.cur = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.cur = Math.ceil((this.min + this.max) / 2);
+  }
 
-    }
+  guess() {
+    return this.cur;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.cur;
+    this.cur = Math.ceil((this.min + this.max) / 2);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.cur;
+    this.cur = Math.ceil((this.min + this.max) / 2);
+  }
 }
 
 module.exports = GuessingGame;
